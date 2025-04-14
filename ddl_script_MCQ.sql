c0b46376cda59d5131a42e9191d64e4a6ee76f94
phuongwhuynh <huynhlanphuong18082004@gmail.com>
2025-04-14 12:26:25 +0700

    Update ddl_script_MCQ.sql

--- ddl_script_MCQ.sql
+++ ddl_script_MCQ.sql
@@ -122,6 +122,7 @@ CREATE TABLE test_attempt(
     FOREIGN KEY(test_id) REFERENCES test(test_id)
 );
 
+
 DROP TABLE IF EXISTS chosen_answer;
 CREATE TABLE chosen_answer(
 	question_id INT UNSIGNED,
@@ -143,5 +144,28 @@ BEGIN
 END$$
 DELIMITER ;
 
-SELECT * from test a JOIN test_have_question b where a.test_id=b.test_id;
-SELECT * from question;
+SELECT 
+	t.test_name AS test_name,
+	q.description,
+	q.ans1,
+	q.ans2,
+	q.ans3,
+	q.ans4,
+	q.correct_answer,
+	ca.answer AS chosen_answer,
+	thq.question_number,
+	tq.total_questions,
+	ta.score
+FROM test_attempt ta
+JOIN test t ON ta.test_id = t.test_id
+JOIN test_have_question thq ON ta.test_id = thq.test_id
+JOIN question q ON thq.question_id = q.question_id
+LEFT JOIN chosen_answer ca 
+	ON ca.question_id = q.question_id AND ca.attempt_id = ta.attempt_id
+JOIN (
+	SELECT test_id, COUNT(*) AS total_questions
+	FROM test_have_question
+	GROUP BY test_id
+) tq ON ta.test_id = tq.test_id
+WHERE ta.attempt_id = 1
+ORDER BY thq.question_number ASC
